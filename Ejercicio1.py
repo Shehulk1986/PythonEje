cdbc208f86e3fc757a9be85ee7375fc21af89aae
Shehulk1986 <yolimatatis781@gmail.com>
2024-11-12 10:08:14 -0500

    cambio4

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,10 +1,29 @@
-#Leer un numero negativo y positivo
-
-Num = int(input("digite el numero: "))
-
-if Num>0:
-    print("el Num positivo: " , Num)
+productos = ["camisas", "pantalones", "blusas" ]
+saldosA = [30, 12, 20]
+compras = [5, 8, 10]
+ventas = [4, 6, 8]
+Nsaldos = 0
+listaF = []
+for i in range (len(productos)):
+      Nsaldos = saldosA[i] + compras[i] - ventas[i]
+      print("el saldo de "+ productos[i], "es", Nsaldos)
+      listaF.append(Nsaldos)
+      print(listaF)
+      input("presione enter ")
+      print("Eso es todo ")
     
-else:
-    print("el Num negativo " , Num)    
     
+    
+#empleados = ["luisa", "pedro", "luisita"]
+#salarios = [8000, 6000, 12000]
+#horas = [20, 12, 8]
+#Nsalarios = 0
+#listaf = []
+#for i in range (len(empleados)):
+    
+ #   Nsalarios = salarios[i] * horas[i]
+ #   
+ #   print("el primer empleado gana: ", Nsalarios)
+ #   listaf.append(Nsalarios)
+ #   print(listaf)
+ #   input("SIGUIENTTE")
--- Ejercicio17.py
+++ Ejercicio17.py
@@ -1,2 +1,25 @@
 #Divisble
 
+a = float(input("ingrese el valor a:"))
+b = float(input("ingrese el valor b:"))
+c = float(input("ingrese el valor c:"))
+
+D = b**2 - 4*a*c
+
+from matd import sqrt
+if D > 0:
+    x1 = (-b + sqrt(D)) / 2*a
+    x2 = (-b - sqrt(D)) / 2*a
+    print("la ecuación cuenta con 2 soluciones reales" , x1, "y , x2")
+    
+elif D ==0:
+    x1 = -b / 2*a
+    print("la ecuación cuenta con una unica solución:", x1)
+    
+else:
+    D < 0
+    ParteReal = -b / (2*a)
+    ParteImaginaria = sqrt(-D) / (2*a)
+    x1 = ParteReal + ParteImaginaria *1j
+    x2 = ParteReal + ParteImaginaria *1j
+    print("la ecuación cuenta con dos soluciones complejas:" , x1, "y" , x2)      
